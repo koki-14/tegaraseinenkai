cf72b5336fdeb846c40cd056c1da4aa3507b7d34
koki <baskouketki@gmail.com>
2026-01-11 12:59:10 +0900

    ファイル追加

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+/* メニュー */
+function toggleMenu() {
+  document.getElementById("sideMenu").classList.toggle("open");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("open");
+}
+
+/* スライドショー */
+const slides = [
+  "img/slide1.jpg",
+  "img/slide2.jpg",
+  "img/slide3.jpg",
+  "img/slide4.jpg"
+];
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  document.getElementById("slideImage").src = slides[index];
+}, 4000);
